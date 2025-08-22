2190e1611b8e670b1a634de97c93cf003f99e152
4xeverburga <ever.burga.p@uni.pe>
2025-08-22 02:08:31 -0500

    chore: update dependency from @nuxt-themes/alpine to @4verburga/alpine-spanishplus@^1.10.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
-  extends: '@nuxt-themes/alpine',
+  extends: '@4verburga/alpine-spanishplus',
 
   modules: [
     // https://github.com/nuxt-modules/plausible
