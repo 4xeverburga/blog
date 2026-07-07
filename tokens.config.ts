3a438dc507455efc9f0500a32d810807e8609ce6
Ever Burga <82112592+4xeverburga@users.noreply.github.com>
2026-07-06 22:06:50 -0500

    feat: point header logo to chiffonstack.kekeros.com and set ChiffonStack red (#e4001f) as accent color (#7)
    - Upgrade @4xeverburga/alpine-spanishplus to 3.1.1-dev.115f299 (adds header.logo.href config)
    - Set alpine.header.logo.href to https://chiffonstack.kekeros.com
    - Override color.primary in tokens.config.ts with a ChiffonStack red (#e4001f) palette

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,19 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    // ChiffonStack brand highlight (#e4001f) as the site's accent color.
+    primary: {
+      50: '#FFE6E9',
+      100: '#FFCCD3',
+      200: '#FF99A7',
+      300: '#FF667A',
+      400: '#FF334E',
+      500: '#E4001F',
+      600: '#B30018',
+      700: '#8F0013',
+      800: '#66000E',
+      900: '#3D0008'
+    }
+  }
 })
